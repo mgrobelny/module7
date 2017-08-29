5e31aa0b3efc94e3dc699e136ad11635f3163f41
mgrobelny <michalmaciejg@gmail.com>
2017-08-29 22:40:11 +0200

    new versions, new files added

--- gruntfile.js
+++ gruntfile.js
@@ -2,21 +2,46 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-	sass: {
+    sass: {
       options: {
         sourceMap: true
       },
       dist: {
         files: {
-          'css/main.css': 'sass/main.sass'
+          'main.css': 'main.sass'
         }
       }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'images/build/'
+        }]
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['sass/*.sass'],
+          tasks: ['sass'],
+          options: {
+            spawn: false,
+        },
+      }
     }
+
 
   });
   // Load the plugins tasks
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ["sass"]);
+
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
 };
